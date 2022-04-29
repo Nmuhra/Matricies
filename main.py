83b6412dddcbab09f54da319201079bbe26b5642
Nabegh <97016646+Nmuhra@users.noreply.github.com>
2022-04-29 22:04:45 +0200

    Create main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import math
 
-dimensions = input("A for : [2*2]*[2*2],B for : [3*3]*[3*3]").upper
+choice = input("A for : [2*2]*[2*2],B for : [3*3]*[3*3] \n").lower()
 
-if dimensions == "A":
+if choice == "a":
   M1C1R1 = int(input("matricie1-column1-row1:\n"))
   M1C1R2 = int(input("matricie1-column1-row2:\n"))
   M1C2R1 = int(input("matricie1-column2-row1:\n"))
@@ -12,4 +12,4 @@ if dimensions == "A":
   M2C1R2 = int(input("matricie2-column1-row2:\n"))
   M2C2R1 = int(input("matricie2-column2-row1:\n"))
   M2C2R2 = int(input("matricie2-column2-row2:\n"))
-  print(f"[({M1C1R1}*{M2C1R1})+({M1C2R1}*{M2C1R2})]")
+  print(f"({M1C1R1}*{M2C1R1})+({M1C2R1}*{M2C1R2})")
